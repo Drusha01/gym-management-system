9e359e8c3696920e72bf3346e78b084242bc383a
Drusha01 <hanz.dumapit53@gmail.com>
2023-02-02 21:37:12 +0800

    login and sign up favicon
    login and sign up favicon

--- login/sign-up.php
+++ login/sign-up.php
@@ -199,7 +199,7 @@ require_once '../classes/users.class.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Keno Gym | Sign-Up</title>
-    <link rel="icon" type="images/x-icon" href="/images/logo.png">
+    <link rel="icon" type="images/x-icon" href="../images/favicon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
     rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
     crossorigin="anonymous">
