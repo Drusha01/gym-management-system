d8c2e815f6cfd9b1b61515e980bbcc5a0c4a2700
Drusha01 <hanz.dumapit53@gmail.com>
2023-02-02 20:50:15 +0800

    user-page
    added php code

--- user/user-page.php
+++ user/user-page.php
@@ -1,6 +1,32 @@
 <?php
+// start session
 session_start();
-print_r($_SESSION);
+
+// includes
+require_once '../tools/functions.php';
+require_once '../classes/users.class.php';
+
+
+// check if we are logged in
+if(isset($_SESSION['user_id'])){
+  // check if the user is active
+  if($_SESSION['user_status_details'] =='active'){
+    // check what type of user are we
+    if($_SESSION['user_type_details'] =='admin'){
+      // go to admin
+      header('location:../admin/admin-profile.php');
+    }else if($_SESSION['user_type_details'] == 'normal'){
+      // do nothing
+    } 
+  }else if($_SESSION['user_status_details'] =='inactive'){
+    // handle inactive user details
+  }else if($_SESSION['user_status_details'] =='deleted'){
+    // handle deleted user details
+  }
+} else {
+  // go to login page
+  header('location:../login/log-in.php');
+}
 
 ?>
 
