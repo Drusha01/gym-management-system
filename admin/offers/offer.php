cdfe358c6706a56a51f45d2a2e352d8e9c2ce370
robroche565 <111483459+robroche565@users.noreply.github.com>
2023-02-09 23:34:49 +0800

    fixed data table

--- admin/offers/offer.php
+++ admin/offers/offer.php
@@ -6,94 +6,69 @@
         <div class="w-100">
             <h5 class="col-12 fw-bold mb-3">Offers</h5>
             <div class="row g-2 mb-2 mt-1">
-                            <div class="col-12 col-sm-3 col-xs-12 form-group table-filter-option">
-                                <label>Type</label>
-                                <select name="sub_type" id="sub_type" class="form-select ms-md-2">
-                                    <option value="">All</option>
-                                    <option value="">Gym-Use Subsciption</option>
-                                    <option value="New Student">Trainer Subscription</option>
-                                    <option value="Shiftee">Locker Subscription</option>
-                                    <option value="Transferee">Program Subscription</option>
-                                </select>
-                            </div>
-                            <div class="col-12 col-sm-4 form-group table-filter-option">
-                            <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Search Name" aria-label="Recipient's username" aria-describedby="basic-addon2">
-                                <div class="input-group-append">
-                                    <button class="btn btn-primary" type="button" style="border-radius:0px 5px 5px 0px"><i class="bx bx-search" style="font-size:18px; vertical-align: middle;"></i></button>
-                                </div>
-                            </div>
+                    <div class="col-12 col-sm-3 col-xs-12 form-group table-filter-option">
+                        <label>Type</label>
+                        <select name="categoryFilter" id="categoryFilter" class="form-select ms-md-2">
+                            <option value="">All</option>
+                            <option value="Gym-Use Subscription">Gym-Use Subscription</option>
+                            <option value="Trainer Subscription">Trainer Subscription</option>
+                            <option value="Locker Subscription">Locker Subscription</option>
+                            <option value="Program Subscription">Program Subscription</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-12 col-sm-4 table-filter-option">
+                        <label for="keyword">Search</label>
+                        <input type="text" name="keyword" id="keyword" placeholder="Enter Name of Offer Here" class="form-control ms-md-2">
+                    </div>
 
-                            </div>
-                            <div class="col-12 col-sm-3 form-group table-filter-option">
-                                <label>Filter</label>
-                                <select name="sub_type" id="sub_type" class="form-select ms-md-2">
-                                    <option value="">Alphabetical</option>
-                                    <option value="">Recent</option>
-                                </select>
-                            </div>
-<<<<<<< Updated upstream
-                            <div class="col-12 col-sm-2 d-grid d-lg-flex form-group h-50 justify-content-lg-center">
-=======
-                            <div class="col-12 col-sm-3 d-grid form-group h-50 justify-content-center">
->>>>>>> Stashed changes
-                                <a href="addoffer.php" class="btn btn-success" role="button">Add Offer</a>
-                            </div>
-                        </div>
-                            <table  class="table table-responsive table-striped table-borderless table-custom" style="overflow-x:auto;">
-                                <thead class="bg-dark text-light">
-                                    <tr>
-                                    <th class="text-center">#</th>
-                                    <th scope="col" class="col-12 col-lg-3">NAME OF OFFER</th>
-                                    <th scope="col" class="text-center ">AGE QUALIFICATION</th>
-                                    <th scope="col" class="text-center ">DAYS</th>
-                                    <th scope="col" class="text-center ">SLOTS</th>
-                                    <th scope="col" class="text-center ">PRICE</th>
-                                    <th scope="col" class="text-center ">ACTION</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                    <th scope="row" class="text-center">1</th>
-                                    <td class="col-12 col-lg-3">1-Month Gym-Use(21 and Above)</td>
-                                    <td class="text-center ">21 above</td>
-                                    <td class="text-center ">30</td>
-                                    <td class="text-center ">None</td>
-                                    <td class="text-center ">₱800.00</td>
-                                    <td class="text-center "><a href="editoffer.php" class="btn btn-primary btn-sm" role="button">Edit</a> <button class="btn btn-danger btn-sm">Delete</button></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            <div class="dataTables_info pb-3 pb-lg-1 text-center text-lg-start" id="table-pending_info" role="status" aria-live="polite">
-                                Showing 1 to 6 of 6 entries
-                            </div>
-                            <div class="dataTables_paginate paging_simple_numbers" id="table-pending_paginate">
-                                <ul class="pagination justify-content-center justify-content-lg-end">
-                                    <li class="paginate_button page-item previous disabled" id="table-pending_previous">
-                                        <a href="#" aria-controls="table-pending" data-dt-idx="previous" tabindex="0" class="page-link">Previous</a>
-                                    </li>
-                                    <li class="paginate_button page-item active">
-                                        <a href="#" aria-controls="table-pending" data-dt-idx="0" tabindex="0" class="page-link">1</a>
-                                    </li>
-                                    <li class="paginate_button page-item">
-                                        <a href="#" aria-controls="table-pending" data-dt-idx="0" tabindex="0" class="page-link">2</a>
-                                    </li>
-                                    <li class="paginate_button page-item">
-                                        <a href="#" aria-controls="table-pending" data-dt-idx="0" tabindex="0" class="page-link">3</a>
-                                    </li>
-                                    <li class="paginate_button page-item next disabled" id="table-pending_next">
-                                        <a href="#" aria-controls="table-pending" data-dt-idx="next" tabindex="0" class="page-link">Next</a>
-                                    </li>
-                                </ul>
-                            </div>
+                    <div class="col-12 col-sm-2 d-grid d-lg-flex form-group h-50 justify-content-lg-center">
+                        <a href="addoffer.php" class="btn btn-success" role="button">Add Offer</a>
+                    </div>
+                </div>
+                <div class="table-responsive table-container">
+
+                </div>
                 </div>
         </div>
 
 
     </main>
-    <script>
+<script>
 
-    </script>
+$.ajax({
+    type: "GET",
+    url: 'offertable.php',
+    success: function(result)
+    {
+        $('div.table-responsive').html(result);
+        dataTable = $("#example").DataTable({
+            "dom": 'rtip',
+            responsive: true,
+            "className": 'details-control',
+            "defaultContent": '<i class = "glyphicon glyphicon-plus-sign"> </ i>',
+            targets: 0
+        });
+        $('input#keyword').on('input', function(e){
+            var status = $(this).val();
+            dataTable.columns([1]).search(status).draw();
+        })
+        $('select#categoryFilter').on('change', function(e){
+            var status = $(this).val();
+            dataTable.columns([2]).search(status).draw();
+        })
+        $('select#program').on('change', function(e){
+            var status = $(this).val();
+            dataTable.columns([3]).search(status).draw();
+        })
+        new $.fn.dataTable.FixedHeader(dataTable);
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) { 
+        alert("Status: " + textStatus); alert("Error: " + errorThrown); 
+    }
+});
+
+
+</script>
 
 </body>
 </html>
---
+++ admin/offers/offertable.php
@@ -0,0 +1,56 @@
+<table id="example"  class="table table-striped table-borderless table-custom">
+    <thead class="bg-dark text-light">
+        <tr>
+        <th class="text-center">#</th>
+        <th scope="col">NAME OF OFFER</th>
+        <th scope="col" class="text-center ">TYPE OF SUBSCRIPTION</th>
+        <th scope="col" class="text-center ">AGE QUALIFICATION</th>
+        <th scope="col" class="text-center ">DAYS</th>
+        <th scope="col" class="text-center ">SLOTS</th>
+        <th scope="col" class="text-center ">PRICE</th>
+        <th scope="col" class="text-center ">ACTION</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <td scope="row" class="text-center">1</td>
+        <td>1-Month Gym-Use(21 and Above)</td>
+        <td class="text-center ">Gym-Use Subscription</td>
+        <td class="text-center ">21 above</td>
+        <td class="text-center ">30</td>
+        <td class="text-center ">None</td>
+        <td class="text-center ">₱800.00</td>
+        <td class="text-center "><a href="editoffer.php" class="btn btn-primary btn-sm" role="button">Edit</a> <button class="btn btn-danger btn-sm">Delete</button></td>
+        </tr>
+        <tr>
+        <td scope="row" class="text-center">2</td>
+        <td>1-Month Trainer</td>
+        <td class="text-center ">Trainer Subscription</td>
+        <td class="text-center ">None</td>
+        <td class="text-center ">30</td>
+        <td class="text-center ">None</td>
+        <td class="text-center ">₱1500.00</td>
+        <td class="text-center "><a href="editoffer.php" class="btn btn-primary btn-sm" role="button">Edit</a> <button class="btn btn-danger btn-sm">Delete</button></td>
+        </tr>
+        <tr>
+        <td scope="row" class="text-center">3</td>
+        <td>1-Month Locker</td>
+        <td class="text-center ">Locker Subscription</td>
+        <td class="text-center ">None</td>
+        <td class="text-center ">30</td>
+        <td class="text-center ">None</td>
+        <td class="text-center ">₱100.00</td>
+        <td class="text-center "><a href="editoffer.php" class="btn btn-primary btn-sm" role="button">Edit</a> <button class="btn btn-danger btn-sm">Delete</button></td>
+        </tr>
+        <tr>
+        <td scope="row" class="text-center">4</td>
+        <td>Zumba</td>
+        <td class="text-center ">Program Subscription</td>
+        <td class="text-center ">21 above</td>
+        <td class="text-center ">30</td>
+        <td class="text-center ">45</td>
+        <td class="text-center ">₱500.00</td>
+        <td class="text-center "><a href="editoffer.php" class="btn btn-primary btn-sm" role="button">Edit</a> <button class="btn btn-danger btn-sm">Delete</button></td>
+        </tr>
+    </tbody>
+</table>
