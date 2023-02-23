76184a97563b3cac0894395c25823e1aff5b0451
robroche565 <111483459+robroche565@users.noreply.github.com>
2023-02-23 16:45:49 +0800

    Madami naggawa
    new avail tabs, walkin expired, subscription
    added modals sa accounts
    may notification na
    settings is edited

--- admin/avail/expiration.php
+++ admin/avail/expiration.php
@@ -10,22 +10,55 @@
                 <option value="Program Subscription">Program Subscription</option>
             </select>
         </div>
+        <div class="form-group col-12 col-sm-3 table-filter-option">
+            <label>Status</label>
+            <select name="categoryFilter-2" id="categoryFilter-2" class="form-select ms-md-2">
+                <option value="">All</option>
+                <option value="Gym-Use Subscription">Nearing</option>
+                <option value="Trainer Subscription">Expired</option>
+            </select>
+        </div>
         <div class="form-group col-12 col-sm-4 table-filter-option">
             <label for="keyword">Search</label>
             <input type="text" name="keyword" id="keyword-2" placeholder="Enter Name Here" class="form-control ms-md-2">
         </div>
-        <div class="col-12 col-sm-3 form-group table-filter-option">
-            <label>Status</label>
-            <select name="filter" id="filter" class="form-select ms-md-2">
-                <option value="">Paid</option>
-                <option value="">Pending</option>
-                <option value="">Partial</option>
-                <option value="">Unpaid</option>
-                <option value="">Overdue</option>
-            </select>
-        </div>
-        <div class="table-responsive table-container table-2">
-
+        <div class="table-responsive table-2">
+            <table id="table-2" class="table table-striped table-borderless table-custom" style="width:100%;border: 3px solid black;">
+                <thead class="bg-dark text-light">
+                    <tr>
+                    <th class="d-lg-none"></th>
+                    <th scope="col" class="text-center d-none d-sm-table-cell">#</th>
+                    <th>NAME</th>
+                    <th class="text-center ">TYPE OF SUBSCRIPTION</th>
+                    <th scope="col" class="text-center">DATE SUBSCRIBED</th>
+                    <th scope="col" class="text-center">DATE EXPIRATION</th>
+                    <th scope="col" class="text-center">DAYS LEFT</th>
+                    <th scope="col" class="text-center">STATUS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th class="d-lg-none"></th>
+                    <th scope="row" class="text-center d-none d-sm-table-cell">1</th>
+                    <td>Trinidad, James Trinidad</td>
+                    <td class="text-center ">Gym-Use Subscription</td>
+                    <td class="text-center">October 16, 2022</td>
+                    <td class="text-center">November 15, 2022</td>
+                    <td class="text-center">4</td>
+                    <td class="text-center">Nearing</td>
+                    </tr>
+                    <tr>
+                    <th class="d-lg-none"></th>
+                    <th scope="row" class="text-center d-none d-sm-table-cell">2</th>
+                    <td>Nicholas, Shania Gabrielle</td>
+                    <td class="text-center ">Gym-Use Subscription</td>
+                    <td class="text-center">October 16, 2022</td>
+                    <td class="text-center">November 15, 2022</td>
+                    <td class="text-center">0</td>
+                    <td class="text-center">Expired</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
--- admin/avail/subscription.php
+++ admin/avail/subscription.php
@@ -1,32 +1,39 @@
 <div class="container">
     <div class="row g-2 mb-2 mt-1">
-        <div class="form-group col-12 col-sm-3 table-filter-option">
-            <label>Type</label>
-            <select name="categoryFilter" id="categoryFilter" class="form-select ms-md-2">
-                <option value="">All</option>
-                <option value="Gym-Use Subscription">Gym-Use Subscription</option>
-                <option value="Trainer Subscription">Trainer Subscription</option>
-                <option value="Locker Subscription">Locker Subscription</option>
-                <option value="Program Subscription">Program Subscription</option>
-                <option value="Program Subscription">Walk-In</option>
-            </select>
-        </div>
-        <div class="form-group col-12 col-sm-4 table-filter-option">
+        <div class="form-group col-12 col-lg-4 table-filter-option">
             <label for="keyword">Search</label>
             <input type="text" name="keyword" id="keyword" placeholder="Enter Name Here" class="form-control ms-md-2">
         </div>
-        <div class="col-12 col-sm-3 form-group table-filter-option">
-            <label>Status</label>
-            <select name="filter" id="filter" class="form-select ms-md-2">
-                <option value="">Paid</option>
-                <option value="">Pending</option>
-                <option value="">Partial</option>
-                <option value="">Unpaid</option>
-                <option value="">Overdue</option>
-            </select>
-        </div>
-        <div class="table-responsive table-container table-1">
-
+        <div class="table-responsive table-1">
+            <table id="table-1" class="table table-bordered table-striped " style="width:100%;border: 3px solid black;">
+                <thead class="table-dark ">
+                    <tr>
+                    <th class="d-lg-none d-sm-none" rowspan="2"></th>
+                    <th class="text-center align-middle d-none d-sm-table-cell" rowspan="2">#</th>
+                    <th class="text-center align-middle" rowspan="2" >NAME</th>
+                    <th class="text-center" colspan="4">SUBSCRIPTION TYPE</th>
+                    <th class="text-center align-middle" rowspan="2">ACTION</th>
+                    </tr>
+                    <tr>
+                    <th class="text-center">Gym-Use</th>
+                    <th class="text-center">Trainer</th>
+                    <th class="text-center">Locker</th>
+                    <th class="text-center">Program</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td class="d-lg-none d-sm-none"></td>
+                    <td class="text-center d-none d-sm-table-cell">1</td>
+                    <td class="text-center">Trinidad, James Lorenz</td>
+                    <td class="text-center">Active</td>
+                    <td class="text-center">Pending</td>
+                    <td class="text-center">Active</td>
+                    <td class="text-center">None</td>
+                    <td class="text-center"><a href="edit-user.php" class="btn btn-primary btn-sm" role="button">Edit</a>  <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">Delete</button></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
