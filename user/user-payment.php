2300ed1f00cae14127d5dd804fcaedc6e5573f9e
Hanrickson E. Dumapit <hanz.dumapit53@gmail.com>
2023-03-17 09:17:00 +0800

    payment
    payment

--- user/user-payment.php
+++ user/user-payment.php
@@ -2,25 +2,26 @@
     <div class="row g-2 mb-2 ">
     <h5 class="col-12 fw-bold">Payment</h5>
         <div class="table-responsive table-1">
-            <table id="table-1" class="table table-striped table-borderless table-custom table-hover" style="width:100%; border: 3px solid black;">
-                <thead class="bg-dark text-light">
-                    <tr>
-                        <th class="d-lg-none"></th>
-                        <th class="text-center">#</th>
-                        <th class="text-center text-uppercase">Payment Description</th>
-                        <th class="text-center text-uppercase">Amount</th>
-                        <th class="text-center text-uppercase">Discount</th>
-                        <th class="text-center text-uppercase">Penalties Due</th>
-                        <th class="text-center text-uppercase">Paid Amount</th>
-                        <th class="text-center text-uppercase">Balance</th>
-                    </tr>
-                </thead>
-                <tbody>
+            
                     <?php 
                     require_once('../classes/subscriptions.class.php');
                     $subscriptionsObj = new subscriptions();
 
                     if($payments_data = $subscriptionsObj->fetch_active_subs_payment($_SESSION['user_id'])){
+                        echo '<table id="table-1" class="table table-striped table-borderless table-custom table-hover" style="width:100%; border: 3px solid black;">
+                        <thead class="bg-dark text-light">
+                            <tr>
+                                <th class="d-lg-none"></th>
+                                <th class="text-center">#</th>
+                                <th class="text-center text-uppercase">Payment Description</th>
+                                <th class="text-center text-uppercase">Amount</th>
+                                <th class="text-center text-uppercase">Discount</th>
+                                <th class="text-center text-uppercase">Penalties Due</th>
+                                <th class="text-center text-uppercase">Paid Amount</th>
+                                <th class="text-center text-uppercase">Balance</th>
+                            </tr>
+                        </thead>
+                        <tbody>';
                         $counter=1;
                         $total_balance = 0;
                         foreach ($payments_data as $key => $value) {
@@ -50,6 +51,19 @@
 
                             // trigger for updating the subscription !!! note that it must paid fully, and days left is negative or 0
                         }
+
+                        echo '     </tbody>
+                        </table>
+                    </div>
+            
+                    <div class="d-flex justify-content-end pe-4">
+                        <p class="fw-bold fs-5">Total Balance: <span class="fw-normal">₱'.number_format($total_balance,2).'</span></p>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <br>
+            <br>';
                     }
                     
                     ?>
@@ -86,15 +100,4 @@
                         <td class="text-center">₱300</td>
                         <td class="text-center">₱100</td>
                     </tr> -->
-                </tbody>
-            </table>
-        </div>
-
-        <div class="d-flex justify-content-end pe-4">
-            <p class="fw-bold fs-5">Total Balance: <span class="fw-normal">₱<?php echo number_format($total_balance,2) ?></span></p>
-        </div>
-    </div>
-</div>
-<br>
-<br>
-<br>
+           
