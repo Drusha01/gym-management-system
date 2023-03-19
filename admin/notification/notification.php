77a7f9d356bf7cee3a64e77fc27d1786044685e6
robroche565 <111483459+robroche565@users.noreply.github.com>
2023-03-19 15:27:10 +0800

    update nmotif

--- admin/notification/notification.php
+++ admin/notification/notification.php
@@ -39,7 +39,7 @@ if(isset($_SESSION['admin_id'])){
 <main class="col-md-9 ms-sm-auto col-lg-9 col-xl-10 p-3 p-md-4">
   <div class="w-100">
     <h5 class="col-12 fw-bold mb-3">Notification</h5>
-    
+        
         <!-- <div class="container py-4">
             <div class="row">
                 <div class="col-lg-12 ">
