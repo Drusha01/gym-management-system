20e320c627849df16970c0d94e9710ce18e749d5
robroche565 <111483459+robroche565@users.noreply.github.com>
2023-03-21 01:27:18 +0800

    Update activate.php
    made it scrollable

--- admin/avail/activate.php
+++ admin/avail/activate.php
@@ -285,7 +285,7 @@ if(isset($_SESSION['admin_id'])){
 </div>
 
 <div class="modal fade" id="add_subs" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-xl">
+  <div class="modal-dialog modal-dialog-centered modal-xl modal-dialog-scrollable">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Add Subscription</h5>
