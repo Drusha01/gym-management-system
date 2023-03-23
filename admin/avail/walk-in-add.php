e88bf3e49e949903aab8f79da999d83e6b920fc0
Drusha01 <hanz.dumapit53@gmail.com>
2023-03-23 19:55:01 +0800

    set
    SET

--- admin/avail/walk-in-add.php
+++ admin/avail/walk-in-add.php
@@ -1,5 +1,4 @@
 <?php 
-session_start();
 if(isset($_SESSION['admin_avail_restriction_details']) && $_SESSION['admin_avail_restriction_details'] == 'Modify'){
 
 }elseif(isset($_SESSION['admin_avail_restriction_details']) && $_SESSION['admin_avail_restriction_details'] == 'Read-Only'){
