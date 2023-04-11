e07af2e7dc10f84c3c2da9f0659d492b762e8e9c
Drusha01 <hanz.dumapit53@gmail.com>
2023-04-11 21:44:22 +0800

    with fancy history
    with fancy history

--- admin/avail/expiration.php
+++ admin/avail/expiration.php
@@ -1,11 +1,13 @@
-<div class="container">
-    <div class="row g-2 mb-2 mt-1">
-        <div class="form-group col-12 col-lg-4 table-filter-option">
-            <label for="keyword">Search</label>
-            <input type="text" name="keyword" id="keyword" placeholder="Enter Name Here" class="form-control ms-md-2">
-        </div>
-        <div class="table-responsive table-container table-2">
-            
+<div class="tab-pane show fade active" id="tab-exp">
+    <div class="container">
+        <div class="row g-2 mb-2 mt-1">
+            <div class="form-group col-12 col-lg-4 table-filter-option">
+                <label for="keyword">Search</label>
+                <input type="text" name="keyword" id="keyword" placeholder="Enter Name Here" class="form-control ms-md-2">
+            </div>
+            <div class="table-responsive table-container table-2">
+                
+            </div>
         </div>
     </div>
 </div>
--- admin/avail/subscription.php
+++ admin/avail/subscription.php
@@ -1,15 +1,17 @@
 
-<div class="container">
-    <div class="row g-2 mb-2 mt-1">
-        <div class="form-group col-12 col-lg-4 table-filter-option">
-            <label for="keyword">Search</label>
-            <input type="text" name="keyword" id="keyword" placeholder="Enter Name Here" class="form-control ms-md-2">
-        </div>
-        <div class="col-12 col-lg-8 d-grid d-lg-inline-flex justify-content-lg-end form-group h-50">
-            <a href="add_avail.php" class="btn btn-success" role="button">Avail Subscription</a>
-        </div>
-        <div class="table-responsive table-container table-1">
-            
+<div class="tab-pane show fade active" id="tab-subs">
+    <div class="container">
+        <div class="row g-2 mb-2 mt-1">
+            <div class="form-group col-12 col-lg-4 table-filter-option">
+                <label for="keyword">Search</label>
+                <input type="text" name="keyword" id="keyword" placeholder="Enter Name Here" class="form-control ms-md-2">
+            </div>
+            <div class="col-12 col-lg-8 d-grid d-lg-inline-flex justify-content-lg-end form-group h-50">
+                <a href="add_avail.php" class="btn btn-success" role="button">Avail Subscription</a>
+            </div>
+            <div class="table-responsive table-container table-1">
+                
+            </div>
         </div>
     </div>
 </div>
