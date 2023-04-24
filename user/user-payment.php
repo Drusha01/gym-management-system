c42f3cc01d156c20a63298be7696f4b3f7ce8702
Drusha01 <hanz.dumapit53@gmail.com>
2023-04-25 02:49:10 +0800

    user payment history, updated to do list

--- user/user-payment.php
+++ user/user-payment.php
@@ -124,7 +124,43 @@
                 </div>
 
                 <div class="table-responsive table-container tbl-hist-paid">
+                    <table id="hist" class="table table-striped table-bordered nowrap" style="width:100%;border: 2px solid grey;">
+                        <thead class="table-light">
+                            <tr>
+                                <th class="d-lg-none"></th>
+                                <th class="text-center">Payment ID</th>
+                                <th class="text-center">Type of Offer</th>
+                                <th class="text-center">Amount Paid</th>
+                                <th class="text-center">Type of Payment</th>
+                                <th class="text-center">Date Paid</th>
+                        
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php 
+                                require_once('../classes/payments.class.php');
+                                $paymentsObj = new payments();
+                                if($payments_data = $paymentsObj->fetch_all_paid_by_user($_SESSION['user_id'])){
+                                    $counter =1;
+                                    foreach ($payments_data as $key => $value) {
+                                        echo '
+                            <tr>
+                                <td class="d-lg-none"></td>
+                                <td class="text-center">'.htmlentities( $value['payment_id']).'</td>
+                                <td class="text-center">'.htmlentities( $value['type_of_subscription_details']).'</td>
+                                <td class="text-center">₱'.htmlentities(number_format($value['payment_amount'],2)).'</td>
+                                <td class="text-center">'.htmlentities( $value['payment_type_details']).'</td>
+                                <td class="text-center">'.htmlentities(date_format(date_create($value['payment_date']), "F d, Y")).'</td>
+                            
+                            </tr>';
+                                    }
+                                }
+                        ?>
+                        
 
+                        
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
